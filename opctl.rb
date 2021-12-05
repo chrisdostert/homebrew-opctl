bda49e010db50f9d11d812c95d1c22e345a85c49
= <=>
2021-12-05 10:55:58 -0800

    f

--- opctl.rb
+++ opctl.rb
@@ -3,7 +3,7 @@ class Opctl < Formula
   homepage "https://opctl.io"
   url "https://github.com/chrisdostert/homebrew-opctl/releases/download/0.0.1/opctl.tar.gz"
   sha256 "4129ad37c0b71f73d144bbd1f2a177839ff4e64d7c460124129df93c9c7ba8bf"
-  depends_on "lima"
+  depends_on "lima", "docker"
   link_overwrite "/usr/local/bin/opctl"
   def install
     bin.install "opctl"
