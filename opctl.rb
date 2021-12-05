33e41a703424e6fb410ec028ee7932902e5aca64
= <=>
2021-12-05 11:34:40 -0800

    f

--- opctl.rb
+++ opctl.rb
@@ -2,7 +2,7 @@ class Opctl < Formula
   desc "Automate operating your project; use containers as building blocks."
   homepage "https://opctl.io"
   url "https://github.com/chrisdostert/homebrew-opctl/releases/download/0.0.1/opctl.tar.gz"
-  sha256 "4129ad37c0b71f73d144bbd1f2a177839ff4e64d7c460124129df93c9c7ba8bf"
+  sha256 "8ff769fc3078d2dfc2ba19fea98f73fc721fe70cee22819b0c8486ba5eb6bd0f"
   depends_on "lima"
   depends_on "docker"
   link_overwrite "/usr/local/bin/opctl"
