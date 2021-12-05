a328ac118b0f0bec0539762bcfa6c5a82bf042e5
Chris Dostert <chrisdostert@users.noreply.github.com>
2021-12-05 13:46:25 -0800

    Update opctl.rb

--- opctl.rb
+++ opctl.rb
@@ -2,7 +2,7 @@ class Opctl < Formula
   desc "Automate operating your project; use containers as building blocks."
   homepage "https://opctl.io"
   url "https://github.com/chrisdostert/homebrew-opctl/releases/download/0.0.1/opctl.tar.gz"
-  sha256 "d2408288ac0846684e5f3ce10cb2600eceebcf922f5e5683f566e4beb8a0dc06"
+  sha256 "d2ca1c10b9fab566c4df38d6ce948e7775a740a54d97db735df8ad9a0eb35022"
   depends_on "lima"
   depends_on "docker"
   link_overwrite "/usr/local/bin/opctl"
